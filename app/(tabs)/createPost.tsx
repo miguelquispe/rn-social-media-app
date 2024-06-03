239fae704f052bf03933a8d48ae169d3934f141a
Miguel Quispe <dev@miguelquispe.com>
2024-06-03 05:02:38 -0500

    feat(front): add image picker to create post screen

--- app/(tabs)/createPost.tsx
+++ app/(tabs)/createPost.tsx
@@ -10,6 +10,8 @@ import {
 } from "react-native";
 import React, { useState } from "react";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
+import { Entypo } from "@expo/vector-icons";
+import * as ImagePicker from "expo-image-picker";
 
 const user = {
   id: "u1",
@@ -20,6 +22,7 @@ const user = {
 
 const CreatePostScreen = () => {
   const [description, setDescription] = useState<string>("");
+  const [image, setImage] = useState<string>();
 
   const insets = useSafeAreaInsets();
 
@@ -28,16 +31,39 @@ const CreatePostScreen = () => {
     setDescription("");
   };
 
+  const handlePickImage = async () => {
+    // No permissions request is necessary for launching the image library
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: false,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.canceled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+
   return (
     <KeyboardAvoidingView
       behavior={Platform.OS === "ios" ? "padding" : "height"}
-      style={[styles.container, { marginBottom: 10 }]}
+      style={[styles.container, { marginBottom: insets.bottom }]}
       contentContainerStyle={{ flex: 1 }}
       keyboardVerticalOffset={150}
     >
       <View style={styles.header}>
         <Image source={{ uri: user.image }} style={styles.profileImage} />
         <Text style={styles.name}>{user.name}</Text>
+        <Entypo
+          onPress={handlePickImage}
+          name="images"
+          size={24}
+          color="limegreen"
+          style={styles.icon}
+        />
       </View>
       <TextInput
         placeholder="What is on your mind?"
@@ -45,6 +71,8 @@ const CreatePostScreen = () => {
         onChangeText={setDescription}
         multiline
       />
+
+      <Image source={{ uri: image }} style={styles.image} />
       <View style={styles.buttonContainer}>
         <Button title="Post" onPress={handleOnSubmit} />
       </View>
@@ -68,12 +96,20 @@ const styles = StyleSheet.create({
     borderRadius: 25,
     marginRight: 10,
   },
+  image: {
+    width: "50%",
+    aspectRatio: 4 / 3,
+    alignSelf: "center",
+  },
   name: {
     fontWeight: "500",
   },
   buttonContainer: {
     marginTop: "auto",
     marginVertical: 10,
+  },
+  icon: {
+    marginLeft: "auto",
   },
 });
 
